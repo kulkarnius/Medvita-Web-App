b499d65d63610ef6e51c0b728df5715d0114cd6e
kulkarnius <64239278+kulkarnius@users.noreply.github.com>
2020-05-26 16:24:28 -0600

    Updated USB.py
    Added request scheme
    Cleaned up string partitioning

--- USB.py
+++ USB.py
@@ -15,7 +15,7 @@ This first block of code reads from the USB and pulls out the value.
 
 #Define Port Structure
 #Make sure baudrate is same as what Sahil has. 
-baudrate = 2400
+baudrate = 4800
 
 #Timeout can be changed it means that the port will be open for 20 seconds or something.
 timeout = 2
@@ -25,32 +25,38 @@ timeout = 2
 dev = serial.serial_for_url('hwgrep://AB0JQF1NA', baudrate=baudrate, timeout=timeout)
 
 #Number of Bytes to be read. Pretty much arbitrary, but it would be good to read more than 20 at least.
-n = 40
+n = 20
+
+#Request the Temperature from the device
+treq = 'T'
+
+#Convert string requests to bytes
+def to_bytes(input):
+    byte = input.encode()
+    return byte
+
+#Define request
+request = to_bytes(treq)
+
+#Sending request to the device
+dev.write(request)
 
 #Read the bytes
 r = dev.read(n)
 #Convert all the bytes to string
 msg = str(r)
 
-#Parse the message to find the specific binary number that is the ADC output
-for r in range(n):
-    if msg[r] == 'n':
-        msg1 = msg[(r+1):(r+11)]
-        break
+#Cut once
+iadcout = msg.partition("'")[2]
 
-#Account for the fact that sometimes the ADC output is only 9 bits.
-for r in range(10):
-    if msg[r] == 'T':
-        adcout = float(msg1[0:10])
-    else:
-        adcout = float(msg1)
-            
+#Cut second time
+adcout = a.partition("T")[0]
 
 #Close Port
 dev.close()
 
 """
-Code picked up from Ryan's table generator for our specific sensor. It gives us the temperature reading.
+Code picked up from Ryan's table generator for our specific sensor. It gives us the temperature reading. The computer has enough processing power to not need the lookup table.
 Written by: Ryan Mah
 Note: assumes Vcc = Vref = 5V
 """

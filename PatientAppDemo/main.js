2b2081b3f455a925e4b01211164af4c783c03d59
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2020-05-28 16:56:02 -0600

    Add files via upload

---
+++ PatientAppDemo/main.js
@@ -0,0 +1,49 @@
+console.log('main process working');
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let parentWindow, childWindow;
+
+function createWindow() {
+    parentWindow = new BrowserWindow({
+        title: 'MedTech', /*show: false,*/
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    });
+    childWindow = new BrowserWindow({
+        parent: parentWindow, modal: true, title: 'Login-Screen',
+        width: 400, maxWidth: 400, height: 525, maxHeight: 525, /*frame: false*/
+    });
+    /*childWindow.once('ready-to-show', () => {
+        childWindow.show()
+    });*/
+    childWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    parentWindow.on('closed', () => {
+        win = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+});

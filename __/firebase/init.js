c2bd38ecb1c82e7e44511b0b121e7b6e6bb0042a
Athul Rajagopal <athulrajagopal2002@gmail.com>
2020-06-11 14:55:54 -0600

    Adding video calling functionality

---
+++ __/firebase/init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALFDCjCeq13KwbnjY7w8YVQwW3Fu69De4",
+    authDomain: "fir-rtc-2002.firebaseapp.com",
+    databaseURL: "https://fir-rtc-2002.firebaseio.com",
+    projectId: "fir-rtc-2002",
+    storageBucket: "fir-rtc-2002.appspot.com",
+    messagingSenderId: "169894595971",
+    appId: "1:169894595971:web:4bfb454f00252866f87ab6",
+    measurementId: "G-GDNNV91E6X"
+  };

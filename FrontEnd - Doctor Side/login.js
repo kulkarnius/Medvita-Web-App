f1315f69560b210ad5adb9b987c5b0075f4d2794
Lucas-Ion <65054476+Lucas-Ion@users.noreply.github.com>
2020-06-12 21:03:10 -0600

    Updated

--- FrontEnd - Doctor Side/login.js
+++ FrontEnd - Doctor Side/login.js
@@ -1,2 +1,29 @@
-var Username = document.getElementById('#Username');
-//alert(Username);
+
+function login(){
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    
+    // if(username == "MedvitaTest" && password == "medvita123")
+    // location.href = "Vitals.html";
+    // else{
+    //     alert("Incorrect Login Details")
+    // }
+
+    var auth = firebase.auth();
+        auth.signInWithEmailAndPassword(username, password).catch(function(error) {
+          alert('Incorrect email or password');
+          return;
+        });
+        
+        auth.onAuthStateChanged(function(user) {
+          if (user) {
+            window.location = "Vitals.html";
+          }
+
+
+    });
+
+}

3119ada84ff1e795404e5aa3fffb736b97a9f28a
Lucas-Ion <65054476+Lucas-Ion@users.noreply.github.com>
2020-06-15 22:57:13 -0600

    Added a waiting room + a lot of updates

--- FrontEnd - Doctor Side/login.js
+++ FrontEnd - Doctor Side/login.js
@@ -1,23 +1,19 @@
-/*
-  Login Function
-  Currently only includes login via email, while
-  HTML says either email or username acceptable
-*/
 
-function login(){
+function login()
+{  
   // Gets user info
   var email = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   console.log(email);
   console.log(password);
 
-  // Logs in user
+  // Signs up the user
   firebase.auth().signInWithEmailAndPassword(email, password)
   .then(function(result) {
     // Redirects to Vitals page
     window.location = "Vitals.html";
   }).catch(function(error) {
     // Error handling
-    alert('Incorrect email or password');
+    alert('Something went wrong');
   });
 }

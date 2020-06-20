342bebb043a74bf1bfd942231cc5ba0c149d9dc3
dpatki <64248134+dpatki@users.noreply.github.com>
2020-06-19 21:24:32 -0600

    Add files via upload

--- serial.js
+++ serial.js
@@ -3,10 +3,14 @@
 //kinda like one of those loops in the c++ books
 //forever lookin for input if you will (calling itself at the end)
 
+// requires
 const SerialPort = require('serialport')
 const prompt = require('prompt');
+//activating port (currently hardcoded to COM3)
 const port = new SerialPort('COM3', {baudRate: 4800})
 
+
+// Janky sleep function lol (TODO: make unjank)
 function sleep(milliseconds) {
   const date = Date.now();
   let currentDate = null;
@@ -15,35 +19,61 @@ function sleep(milliseconds) {
   } while (currentDate - date < milliseconds);
 }
 
+//used for collecting output
+out_array = [];
 
+//Requesting inputs from user - TODO is here
 prompt.start();
-
 prompt.get(['char_input', 'no_of_in'], function (err, result) {
     i = 0;
     while (i < result.no_of_in) {
+      //writing to the port
       port.write(result.char_input, function(err) {
         if (err) {
           return console.log('Error on write: ', err.message)
         }
         console.log('message written')
       })
-      port.on('readable', function () {
-        sleep(400)
-        console.log('Data:', port.read())
-      })
       i++;
     }
 });
 
+// reading from port
+port.on('readable', function () {
+  sleep(400) //optimize this - prevents reading buffer prematurely (very jank though)
+  out_array.push((port.read().toString()));
+  console.log(out_array)
+})
+
+// dunno what this is but it seems important
 function onErr(err) {
     console.log(err);
     return 1;
 }
 
 
+// Exits program after some amout of milliseconds
 setTimeout((function() {
   return process.exit(22);
 }), 5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 rl.question('Input pls ', function(answer) {

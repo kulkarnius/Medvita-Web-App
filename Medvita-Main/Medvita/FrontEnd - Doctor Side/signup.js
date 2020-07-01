3fa02c137b752ad05d7fce780eaa7873f27f823a
JacobLinder <jacob.linder.jl@gmail.com>
2020-06-30 18:00:56 -0700

    Email signup backend for website

--- Medvita-Main/Medvita/FrontEnd - Doctor Side/signup.js
+++ Medvita-Main/Medvita/FrontEnd - Doctor Side/signup.js
@@ -6,6 +6,8 @@
  *       2) Verify complexity of the password
  *       3) Be sure that the TOS checkbox is clicked
  */
+
+/*
 function signup()
 {  
   // Gets user info
@@ -83,4 +85,35 @@ function signup()
     console.log('Error: ', error);
     alert('Could not create an account');
   });
+}*/
+
+// Validates the user's email
+function validateEmail(email) 
+{
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
 }
+
+// Temporary signup for email updates
+function signup() {
+  if (!document.getElementById('invalidCheck').checked) {
+    alert('Please agree to terms and conditions');
+    return;
+  }
+
+  const email = document.getElementById('email').value;
+  if (!validateEmail(email)) {
+    alert('Please enter a valid email address');
+    return;
+  }
+
+  const db = firebase.firestore();
+  db.collection('emails').doc('updates')
+  .update({
+    email: firebase.firestore.FieldValue.arrayUnion(`${email}`)
+  })
+  .then(function() {
+    console.log('Going to next page');
+    window.location = "startup.html";
+  });
+}    
